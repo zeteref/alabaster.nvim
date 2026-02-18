975a80162b0b2af01d32455b2464b3fb7e79216f
zeteref <zeteref@gmail.com>
2026-02-18 09:12:58 +0100

    start working on python treesiter queries

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -11,3 +11,18 @@
 
 (decorator
   (identifier) @AlabasterBase)
+
+(import_statement
+  name: (dotted_name
+    (identifier) @AlabasterVariable (#set! priority 130)))
+ 
+(import_from_statement
+  name: (dotted_name
+    (identifier) @AlabasterVariable (#set! priority 130)))
+
+
+(import_from_statement
+  module_name: (dotted_name
+    (identifier) @AlabasterDefinition (#set! priority 130)))
+ 
+
