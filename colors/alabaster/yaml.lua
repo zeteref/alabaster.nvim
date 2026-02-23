176cb9b3faaf3c4132772ffc79840db6667ec15a
zeteref <zeteref@gmail.com>
2026-02-23 15:21:29 +0100

    make $property bold

--- colors/alabaster/yaml.lua
+++ colors/alabaster/yaml.lua
@@ -5,5 +5,5 @@ return {
   ['@punctuation.delimiter.yaml'] = { fg = '#7577ce' },
   ['@punctuation.bracket.yaml'] = { fg = '#7577ce' },
   --['@key.node.yaml'] = { fg = '#e6db74' },
-  ['@dollar.property.yaml'] = { fg = '#f92672' },
+  ['@dollar.property.yaml'] = { fg = '#f92672', bold = true },
 }
