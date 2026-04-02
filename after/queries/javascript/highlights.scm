2e756becfd5b15360e5a252b7f8b261dd6558f07
zeteref <zeteref@gmail.com>
2026-04-02 14:02:08 +0200

    Add basic highlights for javascript

--- after/queries/javascript/highlights.scm
+++ after/queries/javascript/highlights.scm
@@ -13,3 +13,38 @@
   value: (function_expression))
 
 (undefined) @AlabasterConstant
+
+(function_expression 
+  name: (identifier) @AlabasterDefinition (#set! priority 140))
+
+(function_expression 
+  parameters: (formal_parameters 
+    (identifier) @AlabasterVariableDeclaration (#set! priority 140)))
+
+(assignment_expression 
+  left: (identifier) @AlabasterVariable (#set! priority 140))
+
+(arrow_function 
+  parameters: (formal_parameters 
+    (identifier) @AlabasterVariableDeclaration (#set! priority 140)))
+
+(variable_declarator 
+  name: (identifier) @AlabasterVariableDeclaration (#set! priority 140))
+
+(variable_declarator 
+  name: (identifier) @AlabasterDefinition (#set! priority 140)
+  value: (arrow_function))
+
+(object 
+  (pair 
+    key: (property_identifier) @AlabasterVariable (#set! priority 140)))
+
+(object 
+  (pair 
+    key: (property_identifier) @AlabasterDefinition (#set! priority 140)
+    value: (arrow_function)))
+
+("return" @mykeyword
+  (#set! priority 140))
+
+
