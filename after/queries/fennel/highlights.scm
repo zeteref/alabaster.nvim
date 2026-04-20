4274194c27729a5b997b76939fc84510d1393387
zeteref <zeteref@gmail.com>
2026-04-20 13:48:52 +0200

    add fennel rules

--- after/queries/fennel/highlights.scm
+++ after/queries/fennel/highlights.scm
@@ -1,5 +1,5 @@
-;; vim: ft=query
-;; extends
+; vim: ft=query
+; extends
 
 ; (fn
 ;   name: (symbol) @AlabasterDefinition)
@@ -7,16 +7,58 @@
 ;   ; docstring: (string)? @AlabasterComment
 ; (fn
 ;   name: (multi_symbol (symbol) @AlabasterDefinition))
-
 ; (global
 ;   (binding
 ;     (symbol) @AlabasterDefinition))
-
 (list
   (symbol) @function.macro
   (#match? @function.macro "^macro$")
   (symbol) @AlabasterDefinition)
 
+(lambda_form
+  call: (symbol)
+  name: (symbol) @AlabasterDefinition
+  (#set! priority 140))
+
+(fn_form
+  call: (symbol)
+  name: (symbol) @AlabasterDefinition
+  (#set! priority 140))
+
+(binding_pair
+  lhs: (symbol_binding) @AlabasterVariableDeclaration
+  (#set! priority 140))
+
+(table_binding_pair
+  value: (symbol_binding) @AlabasterVariableDeclaration
+  . ; anchor: must be last
+  (#set! priority 140))
+
+args: (sequence_arguments
+  item: (symbol_binding) @AlabasterVariableDeclaration
+  (#set! priority 140))
+
+(list
+  call: (symbol) @_call
+  (#eq? @_call "tset")
+  . ; anchor: call and item must be adjacent siblings
+  item: (symbol) @AlabasterVariable
+  (#set! priority 140))
+
+(list
+  call: (symbol) @_call
+  (#eq? @_call "set")
+  . ; anchor: call and item must be adjacent siblings
+  item: (symbol) @AlabasterVariable
+  (#set! priority 140))
+
+(set_form
+  call: (symbol)
+  lhs: (multi_symbol
+    base: (symbol_fragment)
+    member: (symbol_fragment) @AlabasterVariable
+    .
+    (#set! priority 140)))
 
 ; ((program . (hashfn) @AlabasterHashbang)
 ;  (#match? @AlabasterHashbang "^#!/")
